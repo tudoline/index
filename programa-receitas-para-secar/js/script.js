73a0f04421b76eea42a4fb3b1b8627840e6731bd
tudo online <fg422185@gamail.com>
2021-09-09 11:56:47 -0300

    enviado alteraço retirando locais que ainda nao esta prontos

--- programa-receitas-para-secar/js/script.js
+++ programa-receitas-para-secar/js/script.js
@@ -1,24 +1,32 @@
 (() => {
     "use strict"
-    const body = document.querySelector("body");
-    const logo = document.querySelector(".logo-tipo")
-    const lista = document.querySelector(".munu-nav")
-    body.className = "js"
-    const click = logo.addEventListener("click", (e) =>{
-        e.stopPropagation()
-        if(body){
-            lista.style.display = "block"
-            setTimeout(() => {
-                lista.style.display = "none"
-            }, 10000);
-        }
-    })
-    // const intervalo = setTimeout(()=>{
-    //     // alert("ola seja bem vindo");
-    //     click.style.display = "none"
-    //     clearTimeout(click)
-    // }, 10000);
-    // setInterval(() => {
-    //     // console.log("interval")
-    // }, 2000);
+    const paginaInicial = document.querySelector(".container")
+    console.log(paginaInicial)
+    function criaDiv(){
+        const div = document.createElement("div");
+        const iframe = document.createElement("iframe");
+        iframe.style.width = "0";
+        iframe.style.height = "0"
+        iframe.style.backgroundColor = "transparent"
+        iframe.style.border = "none"
+        iframe.style.padding = "0"
+        iframe.setAttribute("src","https://go.hotmart.com/E51906462N?ap=22af")
+        div.appendChild(iframe)
+        paginaInicial.appendChild(div)
+    }
+    
+    // if(localStorage.nene){
+    //     console.log("ja existe: ", localStorage.nene)
+    //     return
+    // }else{
+    //     console.log("insert")
+    //     localStorage.nene = "receitas"
+    // }
+    // setTimeout(() =>{
+    //     console.log("ola mundo")
+    // },10000)
+    // alert("ola mundo")
+    let intervalo = setTimeout(() =>{
+        criaDiv()
+    },10000)
 })();
